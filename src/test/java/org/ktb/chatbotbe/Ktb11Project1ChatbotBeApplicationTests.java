968bd67e44f0815da29ad66243dfc46adac036f1
Kongji82 <Kongji4092@gmail.com>
2024-07-15 16:44:01 +0900

    initial commit

---
+++ src/test/java/org/ktb/chatbotbe/Ktb11Project1ChatbotBeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ktb.chatbotbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ktb11Project1ChatbotBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
