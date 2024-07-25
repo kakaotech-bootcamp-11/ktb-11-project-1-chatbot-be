a1287208601b5196b009c2da098cdfac24b69a6a
kongji <kongji4092@gmail.com>
2024-07-25 17:51:09 +0900

    [feat] BaseEntity 추가
    - Time 콜럼 추가할 BaseEntity
    - createdAt, updatedAt, deleteAt

---
+++ src/main/java/org/ktb/chatbotbe/global/common/BaseEntity.java
@@ -0,0 +1,36 @@
+package org.ktb.chatbotbe.global.common;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @Column(insertable = false)
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+    @Column(insertable = false)
+    private LocalDateTime deletedAt;
+
+    protected void delete(LocalDateTime currentTime){
+        if (deletedAt == null){
+            deletedAt = currentTime;
+        }
+    }
+
+    public boolean isDeleted(){
+        return deletedAt != null;
+    }
+}
