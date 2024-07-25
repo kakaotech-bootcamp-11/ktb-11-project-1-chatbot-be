8eedac659fb2050b802191dfbf26e58573a85368
kongji <kongji4092@gmail.com>
2024-07-25 17:51:42 +0900

    [feat] User Entity 추가

---
+++ src/main/java/org/ktb/chatbotbe/domain/user/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package org.ktb.chatbotbe.domain.user.repository;
+
+import org.ktb.chatbotbe.domain.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findBysocialId(String socialId);
+}
