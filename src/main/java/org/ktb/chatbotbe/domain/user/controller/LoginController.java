e0913ed03e25b90c29514d81554e44bab9364be4
kongji <kongji4092@gmail.com>
2024-07-31 14:52:22 +0900

    [bug-fix] socialId String to Long

--- src/main/java/org/ktb/chatbotbe/domain/user/controller/LoginController.java
+++ src/main/java/org/ktb/chatbotbe/domain/user/controller/LoginController.java
@@ -1,16 +1,18 @@
 package org.ktb.chatbotbe.domain.user.controller;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.ktb.chatbotbe.domain.user.entity.User;
+import org.ktb.chatbotbe.domain.user.service.UserService;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class LoginController {
-
+    private final UserService userService;
     @GetMapping("/success")
     public OAuth2User success(@AuthenticationPrincipal OAuth2User user) {
         return user;
@@ -18,6 +20,7 @@ public class LoginController {
 
     @GetMapping("/test")
     public String test(@AuthenticationPrincipal OAuth2User user) {
-        return "test";
+        User temp = userService.findBySocialId(user.getAttribute("id"));
+        return temp.toString();
     }
 }
---
+++ src/main/java/org/ktb/chatbotbe/domain/user/service/UserService.java
@@ -0,0 +1,20 @@
+package org.ktb.chatbotbe.domain.user.service;
+
+import lombok.RequiredArgsConstructor;
+import org.ktb.chatbotbe.domain.user.entity.User;
+import org.ktb.chatbotbe.domain.user.repository.UserRepository;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+
+    public User findBySocialId(Long socilId) {
+        return userRepository.findBysocialId(socilId)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+
+}
