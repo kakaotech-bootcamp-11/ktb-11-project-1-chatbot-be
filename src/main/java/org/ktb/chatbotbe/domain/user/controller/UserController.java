40797573eb5a7d4387475f3d95a91e66ab4f307f
kongji <kongji4092@gmail.com>
2024-10-01 18:16:32 +0900

    [refactor] 주소 등록 api deprecated

--- src/main/java/org/ktb/chatbotbe/domain/user/controller/UserController.java
+++ src/main/java/org/ktb/chatbotbe/domain/user/controller/UserController.java
@@ -1,15 +1,14 @@
 package org.ktb.chatbotbe.domain.user.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.ktb.chatbotbe.domain.user.dto.AddressUpdateRequest;
 import org.ktb.chatbotbe.domain.user.dto.UserInfo;
 import org.ktb.chatbotbe.domain.user.service.UserService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
@@ -25,12 +24,9 @@ public class UserController {
     }
 
     @PostMapping("/address")
-    public ResponseEntity<Map<String, String>> setAddress(@AuthenticationPrincipal OAuth2User user,
-                                                          @RequestBody AddressUpdateRequest requestBody){
-        Long userId = user.getAttribute("id");
-        userService.updateUserAddress(userId, requestBody);
-
-        return ResponseEntity.ok(Map.of("message", "success"));
+    @Deprecated
+    public ResponseEntity<String> setAddress() {
+        return new ResponseEntity<>("This endpoint is deprecated", HttpStatus.GONE);
     }
 
 }
